80c4c359502d8529370c2ebf3242c1f29669b542
okancingoz <okanc.ngoz@gmail.com>
2025-04-19 22:38:01 +0300

    feat: add stack implementation using array and linked list

---
+++ implementations/stack-and-queue/index.js
@@ -0,0 +1,14 @@
+//Stacks
+
+//Browser History
+//google, udemy, youtube
+
+//?Arrays or Linked List? Both
+
+//Queues
+
+//Ticket queue
+
+//Matt -- Joy -- Samir -- Pavel
+
+//?Arrays or Linked List? Linked List
